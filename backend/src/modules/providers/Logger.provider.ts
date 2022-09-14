765a3cdca2c21a1812faa1694d6325970d6e7f16
Erlan Lucio <luciodevinfo@gmail.com>
2022-09-14 05:01:51 -0300

    add create configs server

---
+++ backend/src/modules/providers/Logger.provider.ts
@@ -0,0 +1,32 @@
+import bunyan from 'bunyan';
+import package_info from '../../../package.json';
+
+class LoggerProvider {
+    logger: bunyan;
+
+    _setupLogger() {
+        const logger = bunyan.createLogger({
+            name: package_info.name,
+            streams: [
+                {
+                    stream: process.stdout,
+                    level: bunyan.INFO,
+                },
+            ],
+            Application: package_info.name,
+        });
+
+        this.logger = logger;
+    }
+
+
+    getLogger() {
+        if (!this.logger) {
+            this._setupLogger();
+        }
+
+        return this.logger;
+    }
+}
+
+export default new LoggerProvider();
