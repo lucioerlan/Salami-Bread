c90aab1cf0bcde8370abe365df3b461cc3797fd9
Erlan Lucio <luciodevinfo@gmail.com>
2022-09-15 14:11:06 -0300

    add configs backend

--- backend/ormconfig.ts
+++ backend/ormconfig.ts
@@ -1,16 +1,16 @@
-import { Categories } from './src/infra/entities';
+import { Categories, Products } from './src/infra/entities';
 
 export default {
     type: process.env.DB_TYPE,
     host: process.env.HOST ?? 'localhost',
-    port: process.env.DB_PORT,
+    port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 3306,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     synchronize: process.env.DB_SYNCHRONIZE,
     logging: process.env.DB_LOGGING,
     insecureAuth: true,
-    entities: [Categories],
+    entities: [Categories, Products],
     migrations: ['./src/infra/migrations/*.ts'],
     seeds: ['src/infra/seeding/seeds/**/*{.ts,.js}'],
     factories: ['src/infra/seeding/factories/**/*{.ts,.js}'],
